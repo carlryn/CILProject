14bcced374ef469ea9a4ab5f071b12287bec44e0
Carl Rynegardh <carlry@euler01.hpc-lca.ethz.ch>
2017-06-11 03:46:51 +0200

    Adding processed pics

--- carl/run_restoration.sh
+++ carl/run_restoration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #BSUB -n 24
-#BSUB -W 12:00
+#BSUB -W 11:30
 cd $HOME/git/CILProject/carl
 module load new gcc/4.8.2 python/3.6.0
 python road_renovation_multicore.py --n_processes 24 --data_path ~/git/CILProject/idil/processed --save_dir ~/git/CILProject/data/restoration --n_images 100
