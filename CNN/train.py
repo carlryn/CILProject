e4d1e75f73d8f4dbab99d72a54f8397170b90dd7
idilk <idilk@student.ethz.ch>
2017-07-02 15:47:09 +0200

    VGG16-F1Score

--- CNN/train.py
+++ CNN/train.py
@@ -9,7 +9,7 @@ from skimage.io import imread, imsave
 import pprint
 pp = pprint.PrettyPrinter(depth=6)
 # Load data
-os.environ["CUDA_VISIBLE_DEVICES"] = "2" ##os.environ['SGE_GPU']
+os.environ["CUDA_VISIBLE_DEVICES"] = "1" ##os.environ['SGE_GPU']
 
 class CNN(object):
     def __init__(self, sess, img_height = 400, input_height=128, input_width=128, batch_size=2, output_height=16, output_width=16, input_fname_pattern='*.jpg', checkpoint_dir="checkpoint", sample_dir="samples", name="CIL",dataset=""):
@@ -31,13 +31,27 @@ class CNN(object):
         
     def build_model(self): 
         self.inputs = tf.placeholder( tf.float32, [FLAGS.batch_size, self.input_height, self.input_height, 3], name='input')
-        self.labels = tf.placeholder( tf.float32, [FLAGS.batch_size, self.output_height*self.output_height], name='output')
+        self.labels = tf.placeholder( tf.float32, [FLAGS.batch_size, 1], name='output')
+        self.result = tf.placeholder( tf.float32, [FLAGS.batch_size, 1], name='result')
+        x = tf.Variable([1])
+        def label_0():
+          with tf.control_dependencies([tf.assign(x, [0])]):
+            return tf.identity(x)
+        def label_1():
+          with tf.control_dependencies([tf.assign(x, [1])]):
+            return tf.identity(x)        
         self.model = vgg16(self.batch_size, self.output_height*self.output_height, self.inputs, 'vgg16_weights.npz', self.sess)
+        self.result = tf.reduce_mean(self.model.result, 1)
+        #print df.get_shape()
+        #foreground_threshold =   tf.constant(0.25, shape=[FLAGS.batch_size, 1])
+        #self.result  = tf.cond(df > foreground_threshold, label_1,  label_0)    
+        
         #which loss function to use?
         #try this sometime: tf.nn.weighted_cross_entropy_with_logits
-        self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.model.pred, self.labels))
+        #self.loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(self.result, self.labels))
+        self.loss =tf.reduce_mean(self.labels * -tf.log(self.result) + (1 - self.labels) * -tf.log(1 - self.result))
         loss_sum = tf.summary.scalar("loss", self.loss)
-        
+        print self.loss.get_shape()
         if not os.path.exists(os.path.join(self.checkpoint_dir)):
             os.makedirs(os.path.join(self.checkpoint_dir))
         if not os.path.exists(os.path.join(self.sample_dir,self.name)):
@@ -70,12 +84,21 @@ class CNN(object):
         x = [self.output_height * i for i in range(0, int(path_num))]
         y = x
         print "the coordinates ", x
+        data_label = np.load(os.path.join( "mapOrg","train","labels.npy"))
+        inx = [(name.split(os.path.join(self.dataset_name, "train/"))[1]).split(".jpg")[0] for name in data]
+        inx =np.asarray(inx).astype(int)     
         for epoch in range(0, config.epoch):
             batch_idxs = len(data)// self.batch_size
             for i in x:
                 for j in y:
                     for idx in range(0, batch_idxs):
-                        batch_images, batch_files_label =utils.getBatch(i,j,self.input_height,self.output_height, data[idx*self.batch_size:(idx+1)*self.batch_size],data_label[idx*self.batch_size:(idx+1)*self.batch_size])
+
+                        batch_images =utils.getBatch(i,j,self.input_height,self.output_height, data[idx*self.batch_size:(idx+1)*self.batch_size])
+                        batch_files_label = data_label[inx[idx*self.batch_size:(idx+1)*self.batch_size],i/self.output_height, j/self.output_height]
+                        batch_files_label = np.reshape(batch_files_label, [self.batch_size,-1])
+                        
+                        #print "image num: ",inx[idx*self.batch_size:(idx+1)*self.batch_size],  "pixel coordinate: ", i/self.output_height, j/self.output_height, "pixel label: ", batch_files_label, "im name ", data[idx*self.batch_size:(idx+1)*self.batch_size]                        
+                        
                         err,_, summary_str = self.sess.run([self.loss,self.d_optim, self.summ],
                                                feed_dict={self.inputs: batch_images, self.labels:batch_files_label})
                         self.writer.add_summary(summary_str, counter)
@@ -90,24 +113,26 @@ class CNN(object):
         x = [self.output_height * i for i in range(0, int(path_num))]
         y = x
         data_val = glob(os.path.join(self.dataset_name, "val", self.input_fname_pattern))
-        #glob(os.path.join( "aerialOrg","train","*.jpg"))
-        data_label_val = glob(os.path.join( "mapOrg","val", self.input_fname_pattern))   
+        data_label = np.load(os.path.join( "mapOrg","val","labels.npy"))
+        inx = [(name.split(os.path.join(self.dataset_name, "val/"))[1]).split(".jpg")[0] for name in data_val]
+        inx =np.asarray(inx).astype(int)          
         batch_idxs = len(data_val)// self.batch_size
-        for ind in range(0,batch_idxs):
-            img = np.zeros((self.img_height,self.img_height)).astype(int)
+        img = np.zeros((data_label.shape)).astype(int)
+        for idx in range(0,batch_idxs):
             total_loss = 0 
             for i in x:
                 for j in y:
-                    batch_images, batch_files_label =utils.getBatch(i,j,self.input_height,self.output_height,data_val[ind*self.batch_size:(ind+1)*self.batch_size], data_label_val[ind*self.batch_size:(ind+1)*self.batch_size])
-                    result, loss, summary_str = self.sess.run([self.model.result, self.loss, self.summVal],
+                    batch_images =utils.getBatch(i,j,self.input_height,self.output_height, data_val[idx*self.batch_size:(idx+1)*self.batch_size])
+                    batch_files_label = data_label[inx[idx*self.batch_size:(idx+1)*self.batch_size],i/self.output_height, j/self.output_height]
+                    batch_files_label = np.reshape(batch_files_label, [self.batch_size,-1])
+                    result, loss, summary_str = self.sess.run([self.result, self.loss, self.summVal],
                                                feed_dict={self.inputs: batch_images, self.labels:batch_files_label})
-                    result = np.reshape(result[0,:], [self.output_height,self.output_height])
-                    img[i:i+self.output_height,j:j+self.output_height] = result
+                    img[inx[idx*self.batch_size:(idx+1)*self.batch_size],i/self.output_height, j/self.output_height] = result
                     total_loss += loss
                     self.writer.add_summary(summary_str, epoch)
             total_loss = total_loss/float(len(x)*len(x))       
-            "Test Epoch: [%2d] loss: %.8f"  % (epoch,total_loss)     
-            imsave('./{}/test_{:02d}_{:d}.png'.format(os.path.join(self.sample_dir, self.name), epoch, ind),img)
+            print "Test Epoch: [%2d] loss: %.8f"  % (epoch,total_loss)     
+            np.save('./{}/test_{:02d}_{:d}'.format(os.path.join(self.sample_dir, self.name), epoch, idx),img)
 
             
     def load(self):
@@ -130,17 +155,18 @@ class CNN(object):
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 24, "epoch")
 flags.DEFINE_integer("batch_size", 2, "Batch Size")
-flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
+flags.DEFINE_float("learning_rate", 0.001, "Learning rate of for adam [0.0002]")
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_integer("input_height", 128, "The size of image to use (will be center cropped). [108]")
 flags.DEFINE_integer("output_height", 16, "The size of the output images to produce [64]")
 flags.DEFINE_string("input_fname_pattern", "*.jpg", "Glob pattern of filename of input images [*]")
 flags.DEFINE_string("checkpoint_dir", "checkpoint", "Directory name to save the checkpoints [checkpoint]")
-flags.DEFINE_string("name", "CIL", "the string for experiment [*]")
+flags.DEFINE_string("name", "CILNew", "the string for experiment [*]")
 flags.DEFINE_string("dataset", "aerialOrg", "The name of dataset [aerialOrg]")
 
 FLAGS = tf.flags.FLAGS    
 def main(_):
+    
     pp.pprint(flags.FLAGS.__flags)
     run_config = tf.ConfigProto()
     run_config.gpu_options.allow_growth=True
@@ -154,23 +180,12 @@ def main(_):
             checkpoint_dir=FLAGS.checkpoint_dir,
             name = FLAGS.name,
             dataset = FLAGS.dataset)
-        if cnn.output_height == 8:
-            t_vars = tf.trainable_variables()
-            print [v.name for v in t_vars]
-            cnn.saver = tf.train.Saver(var_list=t_vars, max_to_keep=0)
-            try:
-                tf.global_variables_initializer().run()
-            except:
-                tf.initialize_all_variables().run()
-            counter = 0
-            print cnn.name
-            bol, counter = cnn.load()
-            if bol:
-                cnn.test(0)
-            else:
-                print "ow no"
-        else:    
-            cnn.train(FLAGS)
+        #the code needs the dataset to be in order from 0 to len(data) as the names
+        # you can do that using utils.fuck() but you need the change the names for validation set and train set I was lazy :D
+        #the createLabels gives the 16x16 patch =>{0,1} and saves it in array lbel.npy in mapOrg/... 
+        #I am worried about the loss function, I didnot know what to do
+        #utils.createLabels(os.path.join( "mapOrg","train", cnn.input_fname_pattern), os.path.join( "mapOrg","val", cnn.input_fname_pattern), cnn.img_height, cnn.output_height)
+        cnn.train(FLAGS)
         
 if __name__ == '__main__':
   tf.app.run()
--- CNN/utils.py
+++ CNN/utils.py
@@ -31,24 +31,94 @@ def set_data():
             os.remove(os.path.join("aerialOrg","train","%s.jpg") % str(ind+100*thr))
             os.remove(os.path.join("mapOrg", "train", "%s.jpg") % str(ind + 100 * thr))
 
-def getBatch(i,j,window_size,output_size, path,path_label):
+#def getBatch(i,j,window_size,output_size, path,path_label):
+#    im = imread(path[0])
+#    num = int((window_size - output_size) / 2)
+#    image_patchs = np.zeros((len(path), window_size, window_size,im.shape[2])).astype(int)
+#    image_labels = np.zeros((len(path),output_size*output_size)).astype(int)
+#    for ind,im_path in enumerate(path):
+#        im = imread(im_path)
+#        #pad it with smt that makes more sense
+#        im_pad = np.zeros((num*2+im.shape[0], num*2+int(im.shape[0]),im.shape[2])).astype(int)
+#        im_pad[num:num+im.shape[0],num:num+im.shape[1],:]=im
+#        im_label = imread(path_label[ind])
+#        if len(im_label.shape)>2:
+#            im_label=im_label[:,:,0]
+#            #print "wtf"
+#        batch_files_label = np.reshape(im_label[i:i + output_size, j:j + output_size]/255., [1, -1])
+#
+#        image_patchs[ind]=im_pad[i:i+window_size,j:j+window_size,:]
+#        image_labels[ind] = batch_files_label
+#        #imsave("a.png", image_patchs[ind])
+#        #imsave("b.png",np.reshape(image_labels[ind]*255,[output_size,output_size]))
+#    return image_patchs, image_labels
+
+def getBatch(i,j,window_size,output_size, path):
     im = imread(path[0])
     num = int((window_size - output_size) / 2)
-    image_patchs = np.zeros((len(path), window_size, window_size,im.shape[2])).astype(int)
-    image_labels = np.zeros((len(path),output_size*output_size)).astype(int)
+    image_patchs = np.ones((len(path), window_size, window_size,im.shape[2]))
+    
     for ind,im_path in enumerate(path):
         im = imread(im_path)
         #pad it with smt that makes more sense
         im_pad = np.zeros((num*2+im.shape[0], num*2+int(im.shape[0]),im.shape[2])).astype(int)
+        im_pad[:,:,0] = 88.66068507
+        im_pad[:,:,1] = 87.67575921
+        im_pad[:,:,2] = 77.93427463
         im_pad[num:num+im.shape[0],num:num+im.shape[1],:]=im
-        im_label = imread(path_label[ind])
-        if len(im_label.shape)>2:
-            im_label=im_label[:,:,0]
-            #print "wtf"
-        batch_files_label = np.reshape(im_label[i:i + output_size, j:j + output_size]/255., [1, -1])
-
         image_patchs[ind]=im_pad[i:i+window_size,j:j+window_size,:]
-        image_labels[ind] = batch_files_label
-        #imsave("a.png", image_patchs[ind])
-        #imsave("b.png",np.reshape(image_labels[ind]*255,[output_size,output_size]))
-    return image_patchs, image_labels
+    return image_patchs
+    
+
+
+def createLabels(path, path_val, img_height, output_height):
+    for Currpath in [path, path_val]: 
+        print glob(Currpath)
+        labels = np.zeros((len(glob(Currpath)), img_height/output_height, img_height/output_height))
+        inx = [(name.split(Currpath.split("*")[0])[1]).split(".jpg")[0] for name in glob(Currpath)]
+        inx =np.asarray(inx).astype(int)     
+        print inx
+        for ind, im_path in enumerate(glob(Currpath)):
+            im = imread(im_path)
+            patch_size = output_height
+            for j in range(0, im.shape[1], patch_size):
+                for i in range(0, im.shape[0], patch_size):
+                    patch = im[i:i + patch_size, j:j + patch_size]
+                    labels[inx[ind],i/patch_size,j/patch_size] = patch_to_label(patch)
+            path_ = Currpath.split("*")[0]  
+        print path_
+        np.save(path_+"labels.npy", labels)        
+    
+def patch_to_label(patch):
+   df = np.mean(patch)
+   foreground_threshold = 0.25   
+   if df > foreground_threshold:
+       return 1
+   else:
+       return 0
+    
+def fuck():
+        path_data_train = os.path.join( "aerialOrg","train","ok", "*.jpg")
+        path_map_train = os.path.join( "mapOrg","train","ok", "*.jpg")
+        path_data_val = os.path.join( "aerialOrg","val","ok", "*.jpg")
+        path_map_val = os.path.join( "mapOrg","val","ok", "*.jpg")
+        inx = [(name.split(path_data_train.split("*")[0])[1]).split(".jpg")[0] for name in glob(path_data_train)]
+        inx =np.asarray(inx).astype(int)  
+        inx_map = [(name.split(path_map_train.split("*")[0])[1]).split(".jpg")[0] for name in glob(path_map_train)]
+        inx_map =np.asarray(inx_map).astype(int)
+        print inx_map
+        print path_data_train.split("ok")[0]
+        if np.array_equal(inx_map,inx):
+            count = 1
+            for ind, im_path in enumerate(glob(path_data_train)):
+                print im_path
+                if ind == 12:
+                    im = imread(im_path)
+                    #print glob( path_map_val)[ind]
+                    im_map = imread(glob(path_map_train)[ind])
+                    print str(ind)
+                    imsave(path_data_train.split("ok")[0]+str(ind)+".jpg", im)
+                    #imsave(path_map_train.split("ok")[0]+str(ind)+".jpg", im_map)
+                    count +=count
+        else:
+            "ups"
